593450ad1cfa3433e9a0818280f40639bcc00a25
AonaSuzutsuki <rvvfake.org@gmail.com>
2018-11-20 17:28:54 +0900

    Support to create menu.

--- XMLDocumentToHtmlCUI/XMLDocumentToHtmlCUI/Program.cs
+++ XMLDocumentToHtmlCUI/XMLDocumentToHtmlCUI/Program.cs
@@ -15,13 +15,108 @@ namespace XMLDocumentToHtmlCUI
     {
         static void Main(string[] args)
         {
+
+			//var loader = new Template.TemplateLoader("base_template.html");
+			//loader.Assign("title", "test");
+   //         loader.Assign("body", "test");
+			//Console.WriteLine(loader);
+
             var parser = new CsXmlDocumentParser("base.xml");
-            var members = parser.TreeElement;
-            foreach (var member in members.Namespaces)
-                ShowElements(member);
+			var root = parser.TreeElement;
+			//         foreach (var member in root.Namespaces)
+			//             ShowElements(member);
+			//CreateDirectory(root);
+			//CreateClassFile(root);
+			Console.WriteLine(CreateMenu(root));
 
             Console.ReadLine();
         }
+
+		static void CreateDirectory(Element element, string suffix = "")
+		{
+			if (element != null)
+            {
+                if (element.Namespaces != null)
+                {
+					var name = suffix + element.Name;
+					var di = new DirectoryInfo(name);
+					if (!di.Exists)
+						di.Create();
+                    foreach (var elem in element.Namespaces)
+						CreateDirectory(elem, name + "/");
+                }
+            }
+		}
+
+		static void CreateClassFile(Element element, string suffix = "")
+		{
+			if (element != null)
+            {
+                if (element.Namespaces != null)
+                {
+                    var name = suffix + element.Name;
+                    foreach (var elem in element.Namespaces)
+						CreateClassFile(elem, name + "/");
+                }
+                else
+                {
+                    var name = suffix + element.Name + ".html";
+					var fs = new FileStream(name, FileMode.Create, FileAccess.Write, FileShare.Read);
+					//element.Members: elements
+					WriteHtml(fs, element.Members, element);
+					fs.Dispose();
+                }
+            }
+		}
+
+		static string CreateMenu(Element element, string suffix = "")
+        {
+			if (element.Type == ElementType.Root)
+			{            
+                var sb2 = new StringBuilder();
+				sb2.AppendLine("<ul>");
+                foreach (var elem in element.Namespaces)
+					sb2.AppendLine(CreateMenu(elem, "\t"));
+                sb2.AppendLine("</ul>");
+				return sb2.ToString();
+			}
+
+			if (element == null)
+				return string.Empty;
+
+			var sb = new StringBuilder();
+			if (element.Namespaces != null)
+			{
+				var name = suffix + "<li>" + element.Name;
+				sb.AppendLine(name);
+				sb.AppendLine(suffix + "<ul>");
+				//Console.WriteLine(name);
+				//Console.WriteLine("<ul>");
+				foreach (var elem in element.Namespaces)
+					sb.AppendLine(CreateMenu(elem, suffix + "\t\t"));
+				sb.AppendLine(suffix + "</ul>");
+                sb.AppendLine(suffix + "</li>");
+                //Console.WriteLine("</ul></li>");
+			}
+			else
+			{
+				var name = suffix + "<li><a href=\"#\">" + element.Name + "</a></li>";
+                sb.AppendLine(name);
+				//Console.WriteLine(name);
+			}
+			return sb.ToString();
+		}
+
+		static void WriteHtml(Stream stream, List<Member> members, Element parent)
+		{
+			var classByte = Encoding.UTF8.GetBytes(parent.Name + "\n");
+			stream.Write(classByte, 0, classByte.Length);
+			foreach (var member in members)
+			{
+				var data = Encoding.UTF8.GetBytes(" {0}: {1}\n".FormatString(member.Type.ToString(), member.Name));
+				stream.Write(data, 0, data.Length);
+			}
+		}
 
         static void ShowElements(Element element, string suffix = "")
         {
@@ -63,5 +158,6 @@ namespace XMLDocumentToHtmlCUI
                 }
             }
         }
+
     }
 }
