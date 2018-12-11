222c06549e9e91da1ead99b5ab4e81bd22656948
Aona Suzutsuki <aonasuzutsuki@gmail.com>
2018-12-12 03:55:24 +0900

    changed comments.

--- XMLDocumentToHtmlCUI/XmlDocumentExtensions/Extensions/DictionaryExtension.cs
+++ XMLDocumentToHtmlCUI/XmlDocumentExtensions/Extensions/DictionaryExtension.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 namespace XmlDocumentExtensions.Extensions
 {
     /// <summary>
-    /// Extension functions of <see cref="IDictionary"/>.
+    /// Extension functions of <see cref="Dictionary{TKey, TValue}"/>.
     /// </summary>
     public static class DictionaryExtension
     {
@@ -17,7 +17,7 @@ namespace XmlDocumentExtensions.Extensions
         /// <typeparam name="V">Value type.</typeparam>
         /// <param name="dict">Target dictionary.</param>
         /// <param name="key">The key you want to search</param>
-        /// <param name="defaultValue">Default value/</param>
+        /// <param name="defaultValue">Default value.</param>
         /// <returns></returns>
         public static V Get<K, V>(this Dictionary<K, V> dict, K key, V defaultValue = default)
 		{
@@ -26,7 +26,14 @@ namespace XmlDocumentExtensions.Extensions
 			return defaultValue;
 		}
 
-
+        /// <summary>
+        /// Add only if the specified element does not exist in the dictionary.
+        /// </summary>
+        /// <typeparam name="K">Key type.</typeparam>
+        /// <typeparam name="V">Value type.</typeparam>
+        /// <param name="dict">Target dictionary.</param>
+        /// <param name="key">The key you want to add.</param>
+        /// <param name="value">The value you want to add.</param>
         public static void Put<K, V>(this Dictionary<K, V> dict, K key, V value)
         {
             if (!dict.ContainsKey(key))
