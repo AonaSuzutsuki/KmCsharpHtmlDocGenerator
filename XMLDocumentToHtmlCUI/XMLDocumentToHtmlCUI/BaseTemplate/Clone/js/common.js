678fe9f5a7e45a0dc083c08a3bf348f1eca11796
Aona Suzutsuki <aonasuzutsuki@gmail.com>
2019-01-12 15:21:16 +0900

    support responsive on template.

---
+++ XMLDocumentToHtmlCUI/XMLDocumentToHtmlCUI/BaseTemplate/Clone/js/common.js
@@ -0,0 +1,9 @@
+﻿function registMenu() {
+    $('.menu-trigger').on('click', menuBtClick);
+}
+
+function menuBtClick() {
+    $(this).toggleClass("active");
+    $("#top-menu-bt-cover").toggleClass("top-menu-bt-cover-click");
+    $("#menu").toggleClass("menu-cover-click");
+}
